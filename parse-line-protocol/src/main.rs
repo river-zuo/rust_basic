e50b007fbec73fd2e976f0889748bf7586a69d0e
river-zuo <riverleft@hotmail.com>
2025-03-26 02:10:57 +0800

    添加宏模块

---
+++ parse-line-protocol/src/main.rs
@@ -0,0 +1,8 @@
+
+// <measurement>, <tag_set> <field_set> <time_stamp>
+
+
+fn main() {
+    println!("Hello, world!");
+    
+}
---
+++ rust_macro/src/main.rs
@@ -0,0 +1,27 @@
+macro_rules! add {
+    ($a: expr) => {
+        $a
+    };
+    ($a: expr, $b: expr) => {
+        $a + $b
+    };
+    ($a: expr, $($b: tt)*) => {
+        $a + add!($($b)*)
+    };
+}
+
+#[derive(Debug)]
+pub struct Aaa();
+
+
+fn main() {
+
+    // add!(1, 2, 3, 4);
+    
+    // println!("add!_ {}", add!(1, 2));
+    println!("add!_ {}", add!(1));
+    println!("add!_ {}", add!(1, 2, 4, 5, 6, 7, 8));
+    
+}
+
+
