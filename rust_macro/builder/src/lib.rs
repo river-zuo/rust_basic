8782e0c035133e2ef0e4fd428cfdce16f756468a
river-zuo <riverleft@hotmail.com>
2025-04-17 21:51:18 +0800

    添加宏

--- rust_macro/builder/src/lib.rs
+++ rust_macro/builder/src/lib.rs
@@ -1,6 +1,51 @@
-// use proc_macro::TokenStream;
-// #[proc_macro_derive(Builder)]
-// fn derive_builder(input: )
+use proc_macro::{TokenStream};
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, Type, Data, DeriveInput, Fields, FieldsNamed, Ident};
+use syn::__private::TokenStream2;
+
+fn map_field<F>(fields: &Fields, func: F) -> TokenStream2
+    where 
+    F: FnMut((Ident, Type)) -> TokenStream2
+{   
+    let iter_stream = fields.iter()
+        .map(|f| f.clone())
+        .map(|field| (field.ident.unwrap(), field.ty))
+        .map(func);
+    TokenStream2::from_iter(iter_stream)
+}
+
+#[proc_macro_derive(Builder)]
+pub fn derive_builder(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let ident = input.ident;
+    let builder_ident = format_ident!("{}Builder", ident);
+    
+    if let Data::Struct(data_struct) = input.data {
+        let fields = data_struct.fields;
+        if matches!(fields, Fields::Named(_)) {
+            // let builder_fields = fields.iter()
+            //     .map(|f| f.clone())
+            //     .map(|field| (field.ident.unwrap(), field.ty))
+            //     .map();
+            let build_fields = map_field(&fields, |(ident, ty)| quote! {#ident: Option<#ty>, });
+            let build_set_method = map_field(&fields, |(ident, ty)| quote! {
+                pub fn #ident(mut self, value: #ty) -> Self {
+                    self.#ident = Some(value);
+                    self
+                }
+            });
+        }
+    }
+
+    quote! {
+        impl #ident {
+            pub fn builder() -> #builder_ident {
+                #builder_ident
+            }
+        }
+        pub struct #builder_ident;
+    }.into()
+}
 
 
 // #[cfg(test)]
--- rust_macro/builder/src/main.rs
+++ rust_macro/builder/src/main.rs
@@ -1,54 +1,61 @@
-#[derive(Default)]
-pub struct CommandBuilder {
-    executable: Option<String>,
-    args: Vec<String>,
-    current_dir: Option<String>,
-}
+// #[derive(Default)]
+// pub struct CommandBuilder {
+//     executable: Option<String>,
+//     args: Vec<String>,
+//     current_dir: Option<String>,
+// }
 
-#[derive(Debug)]
+// impl CommandBuilder {
+//     pub fn executable(mut self, exec: String) -> Self {
+//         self.executable = Some(exec);
+//         self
+//     }
+//     pub fn args(mut self, arg: String) -> Self {
+//         self.args.push(arg);
+//         self
+//     }
+//     pub fn current_dir(mut self, dir: String) -> Self {
+//         self.current_dir = Some(dir);
+//         self
+//     }
+
+//     pub fn build(self) -> Result<Command, ()> {
+//         let executable = self.executable.ok_or(())?;
+//         let current_dir = self.current_dir.ok_or(())?;
+//         Ok(Command { executable: executable, args: self.args, current_dir: current_dir })
+//     }
+// }
+
+use builder::Builder;
+
+#[derive(Debug, Builder)]
+// #[derive(Builder)]
+// #[derive(Debug)]
 pub struct Command {
     executable: String, 
     args: Vec<String>,
     current_dir: String,
 }
 
-impl Command {
-    pub fn builder() -> CommandBuilder {
-        CommandBuilder::default()
-    }
-}
-
-impl CommandBuilder {
-    pub fn executable(mut self, exec: String) -> Self {
-        self.executable = Some(exec);
-        self
-    }
-    pub fn args(mut self, arg: String) -> Self {
-        self.args.push(arg);
-        self
-    }
-    pub fn current_dir(mut self, dir: String) -> Self {
-        self.current_dir = Some(dir);
-        self
-    }
-
-    pub fn build(self) -> Result<Command, ()> {
-        let executable = self.executable.ok_or(())?;
-        let current_dir = self.current_dir.ok_or(())?;
-        Ok(Command { executable: executable, args: self.args, current_dir: current_dir })
-    }
-}
+// impl Command {
+//     pub fn builder() -> CommandBuilder {
+//         CommandBuilder::default()
+//     }
+// }
 
 fn main() {
     // let cb = CommandBuilder::default();
     // println!("hello~");
+    
+    // let command = Command::builder()
+    //             .executable("cargo".to_owned())
+    //             .args("--release".to_owned())
+    //             .current_dir("./".to_owned())
+    //             .build()
+    //             .unwrap();
 
-    let command = Command::builder()
-                .executable("cargo".to_owned())
-                .args("--release".to_owned())
-                .current_dir("./".to_owned())
-                .build()
-                .unwrap();
+    // println!("{:?}", command);
+    // let cc = Command::builder();
 
-    println!("{:?}", command);
+
 }
